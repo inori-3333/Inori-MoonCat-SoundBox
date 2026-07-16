a401f1d4fbf48e3c4ed7007f64cf7fa6c5acd121
inori333 <202200130178@mail.sdu.edu.cn>
2026-07-16 08:16:22 +0800

    chore: clear repository for refactor

--- src-tauri/src/core/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod audio;
-pub mod commands;
-pub mod models;
-pub mod scoring;
-pub mod storage;
-pub mod testsuite;
--- src-tauri/src/main.rs
+++
@@ -1,5 +0,0 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    hifi_box_lib::run();
-}
